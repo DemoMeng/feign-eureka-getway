81a950400c09125afa16d1b95f4d2d45e3ef2477
蒙大拿 <977466992@qq.com>
2021-07-30 13:45:08 +0800

    网关重新拼接请求参数

--- src/main/java/com/mqz/feign/getway/filters/GatewayGlobalFilter.java
+++ src/main/java/com/mqz/feign/getway/filters/GatewayGlobalFilter.java
@@ -1,6 +1,8 @@
 package com.mqz.feign.getway.filters;
 
+import cn.hutool.core.util.URLUtil;
 import com.mqz.feign.getway.exception.WithoutLoginException;
+import com.mqz.mars.base.constants.FeignCloudConstant;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
@@ -12,6 +14,7 @@ import org.springframework.util.CollectionUtils;
 import org.springframework.util.MultiValueMap;
 import org.springframework.util.StringUtils;
 import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -69,6 +72,40 @@ public class GatewayGlobalFilter implements GlobalFilter, Ordered {
         }
         Flux<DataBuffer> body = serverHttpRequest.getBody();
         System.out.println(body);
+
+        //拼接请求参数
+        List<String> list = serverHttpRequest.getQueryParams().get(FeignCloudConstant.Gateway.KEY_TOKEN);
+        if(!CollectionUtils.isEmpty(list)){
+            //TODO 校验参数合法性
+            String tokenValue = list.get(0);
+            StringBuilder query = new StringBuilder();
+            //获取请求uri的请求参数（GET请求参数通过拼接key=value形式进行传参）
+            String originalQuery = uri.getRawQuery();
+            //判断最后一个字符是否是&，如果不是则拼接一个&，以备后续的参数进行连接
+            if (StringUtils.hasText(originalQuery)) {
+                query.append(originalQuery);
+                if (originalQuery.charAt(originalQuery.length() - 1) != '&') {
+                    query.append('&');
+                }
+            }
+            query.append(FeignCloudConstant.Gateway.KEY_GATEWAY_WITH);
+            query.append('=');
+            query.append("requestFromGateway");
+            query.append("&s"+FeignCloudConstant.Gateway.KEY_TOKEN);
+            query.append('=');
+            query.append(URLUtil.encode(tokenValue));//hutool 转码
+            String lastQuery = query.toString();
+            //把请求参数重新拼接回去，并放入request中传递到过滤链的下一个请求中去
+            try {
+                URI newUri = UriComponentsBuilder.fromUri(uri).replaceQuery(query.toString()).build(true).toUri();
+                ServerHttpRequest request = exchange.getRequest().mutate().uri(newUri).build();
+                return chain.filter(exchange.mutate().request(request).build());
+            }catch (RuntimeException ex) {
+                throw new IllegalStateException("Invalid URI query: " + query.toString());
+            }
+        }
+
+
         return chain.filter(exchange);
     }
 
